5008a20c0208b9f9f7167b35f7ffa443c47ca5f6
EvanMworia <evannji99@gmail.com>
2024-01-20 18:58:35 +0300

    Initial Commit

---
+++ ArtGalleryFrontend/Models/Auth-Models/RegisterRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtGalleryFrontend.Models.Auth_Models
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        public string FullNames {  get; set; }=string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }=string.Empty;
+        [Required]
+        public string PhysicalAddress { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ ArtGalleryFrontend/Models/Auth-Models/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtGalleryFrontend.Models.Auth_Models
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string FullNames { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        
+    }
+}
