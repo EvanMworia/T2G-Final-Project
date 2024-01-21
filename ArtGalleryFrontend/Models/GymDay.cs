8b9a11684ad285f16f3e9a78dc2b1aeee28f9a81
EvanMworia <evannji99@gmail.com>
2024-01-21 22:29:48 +0300

    Centered divs :-)

---
+++ ArtGalleryFrontend/Models/GymDay.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtGalleryFrontend.Models
+{
+    public class GymDay
+    {
+        public string Day { get; set; } = string.Empty;
+        public string MuscleGroup { get; set; } = string.Empty;
+
+        public string Image { get; set; } = string.Empty;
+    }
+}
