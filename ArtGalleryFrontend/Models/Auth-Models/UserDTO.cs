cadc06e9b0938be067b1f126e5ed48690e29b1f8
EvanMworia <evannji99@gmail.com>
2024-01-23 15:11:24 +0300

    Frontend pages complete

--- ArtGalleryFrontend/Models/Auth-Models/UserDTO.cs
+++ ArtGalleryFrontend/Models/Auth-Models/UserDTO.cs
@@ -4,13 +4,15 @@ namespace ArtGalleryFrontend.Models.Auth_Models
 {
     public class UserDTO
     {
-        public Guid Id { get; set; }
+        // rudisha guid - int ni example
+        public int Id { get; set; }
         public string FullNames { get; set; } = string.Empty;
         [Required]
         public string UserName { get; set; } = string.Empty;
 
         public string Email { get; set; } = string.Empty;
 
+        public string Role { get; set; } = string.Empty;
         
     }
 }
---
+++ ArtGalleryFrontend/UtilityService/UserStateService.cs
@@ -0,0 +1,23 @@
+﻿namespace ArtGalleryFrontend.UtilityService
+{
+    public class UserStateService
+    {
+        public bool IsAdmin { get; private set; }  
+
+        public event Action ? OnChange;
+
+        public void SetRoleUser()
+        {
+            IsAdmin = false;
+            NotifyStateChanged();
+        }
+
+        public void SetRoleAdmin()
+        {
+            IsAdmin = true;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
